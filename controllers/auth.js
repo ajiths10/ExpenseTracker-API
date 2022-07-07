4dea66753603a7b1eb59afc1a5ad57ab49d3dae2
ajiths10 <ajithsnair10@gmail.com>
2022-07-07 23:30:34 +0530

    Already exists user validation added

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,9 +7,14 @@ exports.postSignUp = async (req, res, next) => {
   let name = req?.body?.name ? req.body.name : 0;
   let password = req?.body?.password ? req.body.password : 0;
   let storedHash;
+
   try {
+    const emailExists = await User.findOne({ where: { email: email? email : '' } });
+
     if (!email || !name || !password) {
-      res.json({ message: "Please input correctly!" });
+      res.json({ message: "Please input correctly!", type: 0 });
+    } else if (emailExists) {
+      res.json({ message: "Email already registered" , type: 0 });
     } else {
       //encrypt your password
       const response = await bcrypt.hash(
@@ -18,11 +23,11 @@ exports.postSignUp = async (req, res, next) => {
         function (err, hash) {
           console.log(hash);
           storedHash = hash;
-          User.create({
-            name: name,
-            email: email,
-            password: storedHash,
-          });
+            User.create({
+              name: name,
+              email: email,
+              password: storedHash,
+            })
         }
       );
 
@@ -39,6 +44,7 @@ exports.postSignUp = async (req, res, next) => {
       await res.json({
         message: "User Created successfully",
         response: response,
+        type: 1,
       });
     }
   } catch (err) {
