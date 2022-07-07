477564efe4d785e8b1dc7750277fb8d48461d92c
ajiths10 <ajithsnair10@gmail.com>
2022-07-07 19:47:33 +0530

    SQL user table created

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = 7777;
 
-const sequelize = require('./util/database');
+const sequelize = require("./util/database");
+
+const user = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -14,5 +16,13 @@ app.use(cors());
 console.log("Hello World");
 
 
-app.listen(port);
-console.log(`${port} - port running`)
+sequelize
+  .sync()
+  .then((e) => {
+    // console.log(e);
+    app.listen(port);
+  })
+  .then((e) => console.log(`${port} - port running`))
+  .catch((err) => {
+    console.log(err);
+  });
