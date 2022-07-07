9dbc01d4816e057b4f95d12a3afefd9d204aaaa3
ajiths10 <ajithsnair10@gmail.com>
2022-07-07 22:50:48 +0530

    Password encryption added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,48 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-exports.postSignUp = (req, res, next) => {
+exports.postSignUp = async (req, res, next) => {
   let email = req?.body?.email ? req.body.email : 0;
   let name = req?.body?.name ? req.body.name : 0;
   let password = req?.body?.password ? req.body.password : 0;
+  let storedHash;
+  try {
+    if (!email || !name || !password) {
+      res.json({ message: "Please input correctly!" });
+    } else {
+      //encrypt your password
+      const response = await bcrypt.hash(
+        password,
+        saltRounds,
+        function (err, hash) {
+          console.log(hash);
+          storedHash = hash;
+          User.create({
+            name: name,
+            email: email,
+            password: storedHash,
+          });
+        }
+      );
 
-  if (!email || !name || !password) {
-    res.json({ message: "Please input correctly!" });
-  } else {
-    console.log({ message: { email: email, password: password, name: name } });
-    User.create({
-      email: email,
-      password: password,
-      name: name,
-    })
-      .then((response) => {
-        res.json({
-          message: "User Created successfully",
-          response: response,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json(err);
+      //To check the password
+      //   bcrypt.compare(password, storedHash, function(err, result) {
+      //     res.json({
+      //         message: "User Created successfully",
+      //         response: storedHash,
+      //         result: result,
+      //         check: storedHash == result
+      //       });
+      // });
+
+      await res.json({
+        message: "User Created successfully",
+        response: response,
       });
+    }
+  } catch (err) {
+    console.log(err);
+    res.json(err);
   }
 };
