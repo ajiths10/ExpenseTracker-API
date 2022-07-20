9730dd5c9480d33275b788e74db1f4f3cacb0186
ajiths10 <ajithsnair10@gmail.com>
2022-07-20 23:44:18 +0530

    Download report completed

--- controllers/premium.js
+++ controllers/premium.js
@@ -1,8 +1,8 @@
 const User = require("../models/user");
 const Expenses = require("../models/expenses");
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
-function uploadToS3 (data, filename) {
+function uploadToS3(data, filename) {
   const BUCKET_NAME = process.env.BUCKET_NAME;
   const IAM_USER_KEY = process.env.IAM_USER_KEY;
   const IAM__USER_SECRET = process.env.IAM__USER_SECRET;
@@ -10,25 +10,28 @@ function uploadToS3 (data, filename) {
   let s3bucket = new AWS.S3({
     accessKeyId: IAM_USER_KEY,
     secretAccessKey: IAM__USER_SECRET,
-    //Bucket: BUCKET_NAME 
-  })
+    //Bucket: BUCKET_NAME
+  });
 
-  s3bucket.createBucket(()=>{
-    var params = {
-      Bucket : BUCKET_NAME,
-      Key: filename,
-      Body: data
-    }
+  var params = {
+    Bucket: BUCKET_NAME,
+    Key: filename,
+    Body: data,
+    ACL: "public-read",
+  };
+
+  return new Promise((resolve, reject) => {
     s3bucket.upload(params, (err, s3response) => {
-      if(err) {
-        console.log('Something went wrong', err);
+      if (err) {
+        console.log("Something went wrong", err);
+        reject(err);
       } else {
-        console.log('success', s3response);
+        console.log("success", s3response);
+        resolve(s3response.Location);
       }
-    } )
-  })
+    });
+  });
 }
-
 
 exports.getAllUsers = async (req, res, next) => {
   const allUsers = await User.findAll();
@@ -42,7 +45,7 @@ exports.getAllUsers = async (req, res, next) => {
 exports.postReport = async (req, res, next) => {
   const startDate = req.body.Startdate;
   const endDate = req.body.Enddate;
-  try{
+  try {
     const ExpensesList = await Expenses.findAll({
       where: {
         // createdAt: {
@@ -52,38 +55,37 @@ exports.postReport = async (req, res, next) => {
         userId: req.user.id,
       },
     });
-  
+
     res.json({
       message: "All users fetched",
       response: ExpensesList,
       type: 1,
     });
-
   } catch (err) {
     console.log(err);
     res.json({ message: "Internal Server Error", type: 0, response: err });
   }
 };
 
-exports.reportDownload = async(req, res) => {
+exports.reportDownload = async (req, res) => {
   try {
     const expensesList = await Expenses.findAll({
       where: {
         userId: req.user.id,
       },
-    })
+    });
     const stringifiedExpenses = JSON.stringify(expensesList);
-    const filename = "Expense.txt";
-    const fileUrl = uploadToS3(stringifiedExpenses, filename);
+    const userId = req.user.id;
+    const filename = `Expense${userId}/${new Date()}.txt`;
+    const fileUrl = await uploadToS3(stringifiedExpenses, filename);
+    console.log("====>", fileUrl);
     res.json({
       message: "All users fetched",
       response: fileUrl,
       type: 1,
     });
-
-  
-  }  catch (err) {
+  } catch (err) {
     console.log(err);
     res.json({ message: "Internal Server Error", type: 0, response: err });
   }
-  }
+};
