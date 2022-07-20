7f918f4bd8030c93f58261db64148e8d4b6e26e3
ajiths10 <ajithsnair10@gmail.com>
2022-07-20 22:32:17 +0530

    S3 ceate bucket and upload txt added

--- controllers/premium.js
+++ controllers/premium.js
@@ -1,5 +1,34 @@
 const User = require("../models/user");
 const Expenses = require("../models/expenses");
+const AWS = require('aws-sdk');
+
+function uploadToS3 (data, filename) {
+  const BUCKET_NAME = process.env.BUCKET_NAME;
+  const IAM_USER_KEY = process.env.IAM_USER_KEY;
+  const IAM__USER_SECRET = process.env.IAM__USER_SECRET;
+
+  let s3bucket = new AWS.S3({
+    accessKeyId: IAM_USER_KEY,
+    secretAccessKey: IAM__USER_SECRET,
+    //Bucket: BUCKET_NAME 
+  })
+
+  s3bucket.createBucket(()=>{
+    var params = {
+      Bucket : BUCKET_NAME,
+      Key: filename,
+      Body: data
+    }
+    s3bucket.upload(params, (err, s3response) => {
+      if(err) {
+        console.log('Something went wrong', err);
+      } else {
+        console.log('success', s3response);
+      }
+    } )
+  })
+}
+
 
 exports.getAllUsers = async (req, res, next) => {
   const allUsers = await User.findAll();
@@ -38,6 +67,20 @@ exports.postReport = async (req, res, next) => {
 
 exports.reportDownload = async(req, res) => {
   try {
+    const expensesList = await Expenses.findAll({
+      where: {
+        userId: req.user.id,
+      },
+    })
+    const stringifiedExpenses = JSON.stringify(expensesList);
+    const filename = "Expense.txt";
+    const fileUrl = uploadToS3(stringifiedExpenses, filename);
+    res.json({
+      message: "All users fetched",
+      response: fileUrl,
+      type: 1,
+    });
+
   
   }  catch (err) {
     console.log(err);
